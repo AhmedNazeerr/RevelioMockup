00e128faf0d2eb7afd80111650836d1fe6eaee19
AhmedNazeer <iamahmad9311@gmail.com>
2023-12-16 15:20:22 +0500

    changes added

--- app.js
+++ app.js
@@ -37,7 +37,6 @@ app.use(cors({
 //   })
 // );
 app.use(helmet());
-app.use(cors());
 app.use(xss());
 app.use(mongoSanitize());
 
@@ -53,7 +52,7 @@ app.use('/api/v1/users', userRouter);
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
 
-const port = process.env.PORT || 6000;
+const port = process.env.PORT || 5000;
 const start = async () => {
   try {
     await connectDB(process.env.MONGO_URL);
